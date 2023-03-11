10714bdfa6b0ee029f196883314f25f94ab77d25
Yaroslav <ravlushevich.iarek@mail.ru>
2023-03-11 22:03:48 +0300

    reactotron remove

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import Reactotron from 'reactotron-react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import MyStack from './src/navigation/Stacks';
 
 const App = () => {
-	Reactotron
-		.configure()
-		.useReactNative()
-		.connect()
-
   return (
 		<NavigationContainer>
 			<MyStack />
