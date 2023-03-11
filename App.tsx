8093b6a4bb7f313d836d85a8587b5f552bdf3704
Yaroslav <ravlushevich.iarek@mail.ru>
2023-03-12 00:50:38 +0300

    init flipper debugger

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { initAsyncStorageInspector } from "asyncstorage-inspector-flipper";
+import { debugMobxActions } from 'mobx-action-flipper';
 import MyStack from './src/navigation/Stacks';
+import { userStore } from './src/mobx';
+
+initAsyncStorageInspector();
+debugMobxActions({ userStore });
 
 const App = () => {
   return (
