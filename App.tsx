81d2588230b53425ff0db0ce6530fa756665ee33
Yaroslav <ravlushevich.iarek@mail.ru>
2024-11-01 21:38:03 +0300

    update react-native version to 0.76.0, delete flipper extentsions

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { initAsyncStorageInspector } from "asyncstorage-inspector-flipper";
-import { debugMobxActions } from 'mobx-action-flipper';
 import Stacks from './src/navigation/Stacks';
-import { userStore, institutionsStore, libraryStore, chatStore, quickHelpStore, helpAbroadsStore } from './src/mobx';
-
-initAsyncStorageInspector();
-debugMobxActions({ userStore, institutionsStore, libraryStore, chatStore, quickHelpStore,helpAbroadsStore });
 
 const App = () => {
   return (
