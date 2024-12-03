de83f92447d0221222e4ac1d6dc8b67b7721c854
Yaroslav <ravlushevich.iarek@mail.ru>
2024-12-03 19:39:35 +0300

    rename app, files and folders

--- ios/VstrechaTests/VstrechaTests.m
+++ ios/VstrechaTests/VstrechaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ShoppingListTests : XCTestCase
+@interface VstrechaTests : XCTestCase
 
 @end
 
-@implementation ShoppingListTests
+@implementation VstrechaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
